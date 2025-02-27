62d985ff905ae01c2b2e7dfae90ab89d1e012ba9
Cosimo Casini <casini.cosimo@gmail.com>
2025-02-27 14:50:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const isTuesday = new Date().getDay() === 2;
 document.body.classList.add(isTuesday ? "yes" : "no");
-document.getElementById("text").textContent = isTuesday ? "SI" : "NO";
-document.getElementById("image").src = isTuesday ? "yes-image.png" : "no-image.png";
+document.getElementById("response").textContent = isTuesday ? "SI" : "NO";
+document.getElementById("response-img").src = isTuesday ? "yes-image.png" : "no-image.png";
