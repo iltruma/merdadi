04079f21bf708bb28939c8ba6e3b1cb42ed67661
Cosimo Casini <casini.cosimo@gmail.com>
2025-02-28 15:30:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const isTuesday = new Date().getDay() === 2;
-document.body.classList.add(isTuesday ? "yes" : "no");
-document.getElementById("response").textContent = isTuesday ? "SI" : "NO";
-document.getElementById("response-img").src = isTuesday ? "yes-image.png" : "no-image.png";
+const isFriday = new Date().getDay() === 5;
+
+if (isTuesday) {
+  document.body.classList.add("yes");
+  document.getElementById("response").textContent = "SI"
+  document.getElementById("response-emoji").textContent = "🌈"
+  document.getElementById("response-img").src = "https://imgur.com/a/RVQqLM2"
+} else if (isFriday) {
+  document.body.classList.add("no");
+  document.getElementById("response").textContent = "NO, ED È PURE VENERDÌ"
+  document.getElementById("response-emoji").textContent = "🎉"
+  document.getElementById("response-img").src = "https://tenor.com/it/view/friday-lando-norris-f1-mclaren-gif-18574850"
+} else {
+  document.body.classList.add("no");
+  document.getElementById("response").textContent = "NO"
+  document.getElementById("response-emoji").textContent = "🍀"
+  document.getElementById("response-img").src = "https://tenor.com/it/view/dont-cry-everything-i-sfine-everything-is-gonna-be-alright-dog-burning-house-gif-11300965"
+}
+
